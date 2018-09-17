be56d55913605f7afe9ed08eda0981fc1cc8e6d0
cchampio1964 <cchampio@nmsu.edu>
2018-09-17 16:25:17 -0600

    Moving untilites to package

---
+++ utility/utility_functions.R
@@ -0,0 +1,31 @@
+#the basis for these equations is from http://terraref.org/articles/lemnatec-scanalyzer-field-sensors/
+# key data is that the camera has a 380-1000 nm @ 2/3 nm resolution
+#potental issue is that 620 (1000-380) has 930 incriments, have 25 extra
+
+#' convert wavelength (nm) into an index number for the terraref dataset.
+#' 
+#' @param wavelength A number in nm
+#'
+#' @examples 
+#' wavelengthtoindex(450)
+wavelengthtoindex <- function(wavelength){
+  index <- (wavelength-380)/(2/3)
+  index <- round(index)
+  return(index)
+}
+
+#' convert an index into a wavelength for the terraref dataset.
+#' 
+#' @param index A number 
+#'
+#' @examples 
+#' indextowavelength(450)
+indextowavelength <- function(index){
+  wavelength <- (index*(2/3))+380
+  wavelength <- round(wavelength)
+  return(wavelength)
+}
+
+
+
+
