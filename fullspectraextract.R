5852553c8f8339e27053990ad6ca874fc2fef740
cchampio1964 <cchampio@nmsu.edu>
2018-09-18 12:07:41 -0600

    added min max scaling

--- fullspectraextract.R
+++ fullspectraextract.R
@@ -7,10 +7,11 @@ files <- list.files(pattern = "*.nc")
 finaldata <- data.frame()
 i <- 1
 
-for (i in 1:length(files)){
+for (i in 1:length(files)) {
   first <- 0
   ii <- 1
-  x <- raster(files[i], varname = "rfl_img", band = wavelengthtoindex(665))
+  x <-
+    raster(files[i], varname = "rfl_img", band = wavelengthtoindex(665))
   #there is a calibrarion card in the image, this removes it
   #e <- extent(202.8447, 204.4113, 7.312396, 7.84031)
   #x <- crop(x, e)
@@ -18,7 +19,6 @@ for (i in 1:length(files)){
   x1 <- scale(as.vector(x))
   #if a missing vaule is generaten replace with mean of dataset
   x1 <- ifelse(is.na(x1), mean(x1, na.rm = TRUE), x1)
-  x1 <- na.omit(x1)
   
   #if no variation in data then all values will be NA after scalling
   if (length(x1) > 0) {
@@ -47,12 +47,28 @@ for (i in 1:length(files)){
       data <- tmp
       first <- 1
     }
-    print(paste(round((i/length(files))*100), "% of files and (", round((ii/995)*100), "%) of bands done", sep = ""))
+    
+    if (ii == 955) {
+      data$intense <-
+        (data$intense  - min(data$intense)) / (max(data$intense) - min(data$intense))
+    }
+    
+    print(paste(
+      round((i / length(files)) * 100),
+      "% of files and (",
+      round((ii / 995) * 100),
+      "%) of bands done",
+      sep = ""
+    ))
     
   }
   
-  colnames(data) <- c("index",paste("intense", i, sep = "."))
+  colnames(data) <- c("index", paste("intense", i, sep = "."))
   
-  ifelse(length(finaldata) == 0, finaldata <- data, finaldata <- merge(finaldata, data, by="index"))
+  ifelse(
+    length(finaldata) == 0,
+    finaldata <- data,
+    finaldata <- merge(finaldata, data, by = "index")
+  )
   
 }
